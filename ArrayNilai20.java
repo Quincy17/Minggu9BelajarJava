d1151eb2f030f5f62fc5c09f0aebdd64398b505d
FarrelCaesarian <farrelcaesarian15@gmail.com>
2023-10-24 13:24:04 +0700

    Part 1

---
+++ ArrayNilai20.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ArrayNilai20{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int [] nilaiAkhir = new int[10];
+
+        for(int i=0;i<10;i++){
+            System.out.print("Masukkan Nilai Akhir ke " +i+ " : ");
+            nilaiAkhir[i]= input.nextInt();
+        }
+        for(int i=0;i<10;i++){
+            System.out.println("Nilai Akhir ke-"+i+" adalah " +nilaiAkhir[i]);
+        }
+    }
+}
